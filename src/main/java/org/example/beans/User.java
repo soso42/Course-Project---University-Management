080b96429bd1dbfb15ef3eb0edb21b91c60230d2
Soso42 <sosojanashvili@gmail.com>
2022-12-15 20:20:37 +0400

    Added User class

---
+++ src/main/java/org/example/beans/User.java
@@ -0,0 +1,16 @@
+package org.example.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+}
