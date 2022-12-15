14e0d3908796788a76d2deb4bbc9a8d0a360b06f
Soso42 <sosojanashvili@gmail.com>
2022-12-15 20:21:06 +0400

    Added exceptions

---
+++ src/main/java/org/example/exceptions/CourseNotFoundException.java
@@ -0,0 +1,9 @@
+package org.example.exceptions;
+
+public class CourseNotFoundException extends Exception {
+
+    public CourseNotFoundException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/org/example/exceptions/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package org.example.exceptions;
+
+public class StudentNotFoundException extends Exception {
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+}
